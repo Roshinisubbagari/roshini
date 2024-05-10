b35e81af3714b3314a8fb94e3b05bce2818914b9
Roshinisubbagari <roshini.subbagari@jukshio.com>
2024-05-10 17:03:43 +0530

    Add new code

--- histog.py
+++ histog.py
@@ -1,13 +1,20 @@
 import numpy as np
-import cv2 as cv
+import cv2
 from matplotlib import pyplot as plt
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('image', help='enter the image path')
+parser.add_argument('output', help='enter the output path')
+
+args = vars(parser.parse_args())
+image = cv2.imread(args['image'])
+
  
-img = cv.imread('/home/roshini-subbagari/Desktop/archana/lily.jpeg')
-cv.imwrite("/home/roshini-subbagari/Desktop/archana/lil.jpeg",img)
-assert img is not None, "file could not be read, check with os.path.exists()"
+assert image is not None, "file could not be read, check with os.path.exists()"
 color = ('b','g','r')
 for i,col in enumerate(color):
- histr = cv.calcHist([img],[i],None,[256],[0,256])
+ histr = cv2.calcHist([image],[i],None,[256],[0,256])
  plt.plot(histr,color = col)
  plt.xlim([0,256])
 plt.show()
